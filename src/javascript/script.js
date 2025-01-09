e0012652a2bfb074c941c28e3ae90a9857ac8196
filipe <felepesilvagalvao@gmail.com>
2025-01-09 09:51:30 -0300

    add changes

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -98,3 +98,21 @@ a.forEach((link)=>{
     })
 })	
 
+
+jQuery(document).ready(function () {
+
+    jQuery(window).scroll(function () {
+        if (jQuery(this).scrollTop() > 100) {
+            jQuery('a[href="#top"]').fadeIn();
+        } else {
+            jQuery('a[href="#top"]').fadeOut();
+        }
+    });
+
+    jQuery('a[href="#top"]').click(function () {
+        jQuery('html, body').animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+
+});	
+
